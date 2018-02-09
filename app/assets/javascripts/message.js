8653ab347a4d6df9f7d59a82963dbe4802ec9d8a
稲員未来 <miku.inakazu@gmail.com>
2018-02-09 16:03:22 +0900

    installed unicorn

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,81 +1,83 @@
 $(function() {
-  function buildHTML(message){
-    var image = message.image? `<img src="${message.image}">` : "";
-    var html = `<div class="chat_screen__content" data-id="${message.id}">
-                    <p class="chat_screen__content--name">
-                    ${message.user_name}
-                    </p>
-                    <p class="chat_screen__content--date">
-                    ${message.date}
-                    </p>
-                    <p class="chat_screen__text">
-                    ${message.content}
-                    </p>
-                    <p class="chat_screen__image">
-                    ${image}
-                    </p>
-                  </div>`
-    return html;
-  }
+  $(document).on('turbolinks:load', function() {
 
-  function autoReload(){
-    var lastContent = $('.chat_screen__content').last();
-    var lastId = lastContent.data('id');
+    function buildHTML(message){
+      var image = message.image? `<img src="${message.image}">` : "";
+      var html = `<div class="chat_screen__content" data-id="${message.id}">
+                      <p class="chat_screen__content--name">
+                      ${message.user_name}
+                      </p>
+                      <p class="chat_screen__content--date">
+                      ${message.date}
+                      </p>
+                      <p class="chat_screen__text">
+                      ${message.content}
+                      </p>
+                      <p class="chat_screen__image">
+                      ${image}
+                      </p>
+                    </div>`
+      return html;
+    }
 
-    var url = location.href;
-    if (!(url.includes('groups' && 'messages'))){
-      return
-    };
+    function autoReload(){
+      var lastContent = $('.chat_screen__content').last();
+      var lastId = lastContent.data('id');
 
-    $.ajax({
-      url: location.href,
-      type: 'GET',
-      data: {
-        last_id: lastId
-      },
-      dataType: 'json'
-    })
-    .done(function(data) {
-      data.forEach(function(datum) {
-        var html = buildHTML(datum);
-        $('.chat_screen').append(html)
-      })
-    })
-    .fail(function() {
-      alert('error');
-    })
-  }
+      var url = location.href;
+      if (!(url.includes('groups' && 'messages'))){
+        return
+      };
 
-
-  setInterval(function() {
-    autoReload();
-  }, 5000);
-
-  var url = location.href;
-  if (url.includes('groups' && 'messages')){
-    $('form').on('submit', function(e) {
-      console.log('成功！！！')
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
       $.ajax({
-        url: url,
-        type: 'POST',
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
+        url: location.href,
+        type: 'GET',
+        data: {
+          last_id: lastId
+        },
+        dataType: 'json'
       })
       .done(function(data) {
-        var html = buildHTML(data);
-        $('.chat_screen').append(html);
-        $('form').val('');
-        $('.chat_screen').animate({ scrollTop:$('.chat_screen')[0].scrollHeight });
+        data.forEach(function(datum) {
+          var html = buildHTML(datum);
+          $('.chat_screen').append(html)
+        })
       })
       .fail(function() {
         alert('error');
       })
-    })
-  };
-})
+    }
+
+
+    setInterval(function() {
+      autoReload();
+    }, 5000);
+
+    var url = location.href;
+    if (url.includes('groups' && 'messages')){
+      $('form').on('submit', function(e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+          url: url,
+          type: 'POST',
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data) {
+          var html = buildHTML(data);
+          $('.chat_screen').append(html);
+          $('.input_screen__text').val('');
+          $('.chat_screen').animate({ scrollTop:$('.chat_screen')[0].scrollHeight });
+        })
+        .fail(function() {
+          alert('error');
+        });
+      });
+    };
+  });
+});
 
