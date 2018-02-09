90cf34d3ddd076a7f68d78a31d33dbb37c93eaa3
稲員未来 <miku.inakazu@gmail.com>
2018-02-09 18:56:19 +0900

    modified secret key path

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/inakazumiku/.ssh//var/www/chat-space']
+                  keys: ['/Users/inakazumiku/.ssh//Itoyono3690.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
